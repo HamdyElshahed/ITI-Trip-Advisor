22290e23b04fee1f45b118df383c40a76ad2f11b
Hassan Gad <hassangelrab@gmail.com>
2021-12-01 01:03:35 +0200

    switch betwen carousel view & listview upon filter

--- src/app/components/restaurant/restaurants/restaurants.component.ts
+++ src/app/components/restaurant/restaurants/restaurants.component.ts
@@ -12,11 +12,13 @@ export class RestaurantsComponent implements OnInit {
  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
  filterBy:string[];
  filterByCommonFeatur:string[];
+ showCarousel:boolean;
 
  restaurants!:Restaurant[];
  constructor(private restaurantService:RestaurantsMainServiceService,private filterService:FilterService) { 
   this.filterBy= ["Features","Neighborhoods","Categories",];
   this.filterByCommonFeatur= ["Delivery","Outdoor Seating","Reservations","Dinner","Good for Groups"];
+  this.showCarousel=true;
  }
 
   ngOnInit(): void {
@@ -27,6 +29,7 @@ export class RestaurantsComponent implements OnInit {
 
     this.filterService.filterUpdated.subscribe(val=>{
       this.restaurants=val;
+      this.showCarousel=false;
     })
   }
 
