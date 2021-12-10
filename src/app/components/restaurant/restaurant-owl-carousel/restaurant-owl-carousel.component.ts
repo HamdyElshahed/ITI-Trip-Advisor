1d207529c9328302ffa939bc549121ddb70110ba
Hassan Gad <hassangelrab@gmail.com>
2021-12-10 16:45:17 +0200

    fix styling issues

--- src/app/components/restaurant/restaurant-owl-carousel/restaurant-owl-carousel.component.ts
+++ src/app/components/restaurant/restaurant-owl-carousel/restaurant-owl-carousel.component.ts
@@ -14,7 +14,7 @@ export class RestaurantOwlCarouselComponent implements OnInit {
   constructor(private filterService:FilterService) { }
 
   ngOnInit(): void {
-    
+
   }
 
 
@@ -30,21 +30,21 @@ export class RestaurantOwlCarouselComponent implements OnInit {
    responsive: {
     0: {
       items: 2,
-      
+
   },
   600: {
       items: 3,
-      
+
   },
   800: {
       items: 4,
-      
+
   },
   1000: {
       items: 5,
   }
    },
-  
+  margin:5
  }
 
 }
