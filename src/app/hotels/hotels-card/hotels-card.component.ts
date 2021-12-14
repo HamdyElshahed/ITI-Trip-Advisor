37a9d2fbbebfb6056df23662fb9eec91583039f8
Alaaelwardany <lokaelwardany@gmail.com>
2021-12-14 04:13:50 +0200

    pagination

--- src/app/hotels/hotels-card/hotels-card.component.ts
+++ src/app/hotels/hotels-card/hotels-card.component.ts
@@ -1,5 +1,5 @@
 import { hotels } from './../../models/hotels.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HotelsServiceService } from 'src/app/service/hotels-service.service';
 import { TranslateService } from '@ngx-translate/core';
 @Component({
@@ -8,25 +8,27 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./hotels-card.component.scss'],
 })
 export class HotelsCardComponent implements OnInit {
-  hotels: hotels[] = [];
-  Id: string[] = [];
+  @Input() hotels!: hotels | any;
+  // @Input() Id: string;
+  //  @ hotels: hotels[] = [];
+  // Id: string[] = [];
   constructor(
     private hs: HotelsServiceService,
     public Translate: TranslateService
   ) {}
 
   ngOnInit(): void {
-    this.getallhotels();
+    // this.getallhotels();
   }
-  getallhotels() {
-    this.hs.getallhotels().subscribe((res: any) => {
-      this.hotels = res;
-      for (let i = 0; i < res.length; i++) {
-        this.Id.push(res[i].Id);
-      }
-      console.log(res.length);
-      console.log(this.Id);
-      console.log(res[0].Id);
-    });
-  }
+  // getallhotels() {
+  //   this.hs.getallhotels().subscribe((res: any) => {
+  //     this.hotels = res;
+  //     for (let i = 0; i < res.length; i++) {
+  //       this.Id.push(res[i].Id);
+  //     }
+  //     console.log(res.length);
+  //     console.log(this.Id);
+  //     console.log(res[0].Id);
+  //   });
+  // }
 }
--- src/app/hotels/hotels/hotels.component.ts
+++ src/app/hotels/hotels/hotels.component.ts
@@ -1,6 +1,7 @@
 import { HotelsServiceService } from 'src/app/service/hotels-service.service';
 import { hotels } from './../../models/hotels.model';
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hotels',
@@ -8,7 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hotels.component.scss'],
 })
 export class HotelsComponent implements OnInit {
-  constructor() {}
+  currentPage: number = 0;
+  noOpages: number[] = [];
+  hotels: hotels[] = [];
+  toBeViwehotels: hotels[] = [];
+  // Id: string[] = [];
+  pageSize = 5;
+  constructor(
+    private hs: HotelsServiceService,
+    public Translate: TranslateService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getallhotels();
+  }
+  getallhotels() {
+    this.hs.getallhotels().subscribe((res: any) => {
+      this.hotels = res;
+      // for (let i = 0; i < res.length; i++) {
+      //   this.Id.push(res[i].Id);
+      // }
+      console.log(this.hotels);
+      this.paging();
+      this.sliceArray();
+      console.log(res.length);
+      console.log(res[0].Id);
+    });
+  }
+
+  paging() {
+    const numberOfPages = Math.ceil(this.hotels.length / this.pageSize);
+    for (let i = 0; i < numberOfPages; i++) {
+      this.noOpages.push(i + 1);
+    }
+  }
+
+  sliceArray() {
+    this.toBeViwehotels = this.hotels.slice(
+      this.currentPage * this.pageSize,
+      this.currentPage * this.pageSize + this.pageSize
+    );
+  }
+
+  onPagenation(i: number) {
+    if (i > -1 && i < this.noOpages.length) {
+      this.currentPage = i;
+      this.sliceArray();
+    }
+  }
 }
