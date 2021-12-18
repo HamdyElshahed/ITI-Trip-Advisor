d9ac1a65320ce30a8191d7fce474e422a0466bef
Hassan Gad <hassangelrab@gmail.com>
2021-12-18 02:05:53 +0200

    add fav to detailed card

--- src/app/components/restaurant/detailed-card/detailed-card.component.ts
+++ src/app/components/restaurant/detailed-card/detailed-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Restaurant } from 'src/app/models/restaurant.model';
+import { ProfileService } from 'src/app/services/profile.service';
+import { FilterService } from 'src/app/services/restaurant-services/filter.service';
 
 @Component({
   selector: 'app-detailed-card',
@@ -7,13 +9,44 @@ import { Restaurant } from 'src/app/models/restaurant.model';
   styleUrls: ['./detailed-card.component.scss']
 })
 export class DetailedCardComponent implements OnInit {
-@Input() restaurant!:Restaurant;
-currentRate!:number;
-
-  constructor() { }
+  @Input() restaurant!: Restaurant;
+  currentRate!: number;
+  isFav: boolean = false;
+  favorites: any[] = [];
+  constructor(private profileService: ProfileService, private filterService: FilterService) { }
 
   ngOnInit(): void {
-    this.currentRate=this.restaurant.rating?this.restaurant.rating:0;
+    this.currentRate = this.restaurant.rating ? this.restaurant.rating : 0;
+    this.profileService.userDataObs.subscribe(val => {
+      val.favorites.forEach(val => {
+        if (val['uid'] == this.restaurant.id) {
+          this.isFav = true;
+        }
+        // console.log(val['uid']);
+      })
+      this.favorites = val.favorites;
+    })
+
   }
 
+  addToFavorites() {
+    let favListItemes: any[] = [];
+    if (this.isFav) {
+      this.isFav = false;
+
+      this.favorites.forEach(val => {
+        if (val['uid'] != this.restaurant.id) {
+          favListItemes.push({ "uid": val['uid'] })
+        }
+      }
+      )
+
+      console.log(favListItemes);
+      this.profileService.deleteFavorites(favListItemes);
+    } else {
+      this.profileService.updateFavorites(this.restaurant.id);
+    }
+  }
 }
+
+
